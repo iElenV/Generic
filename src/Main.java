a0d7d9cce6ef8ce15ad20509a89e22c06deb823e
iElenV <energyzer_85@mail.ru>
2023-12-21 21:03:44 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,100 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-       Integer integer = 5;
-       Optional <Integer> optional = new Optional<>(integer);
-       Double doubleVal = 5.0;
-       Optional <Double> optional2 = new Optional<>(doubleVal);
+//        Integer integer = 5;
+//     Optional <Integer> optional = new Optional<>(integer);
+//       Double doubleVal = 5.0;
+//       Optional <Double> optional2 = new Optional<>(doubleVal);
+//
+//      Integer o = optional.get();
+//     System.out.println(o);
 
-       Integer o = optional.get();
-        System.out.println(o);
+//       Integer [] array = new Integer[4];
+//
+//
+//        ArrayList <Integer> list = new ArrayList<>();
+//        ArrayList <Integer> from1to100 = new ArrayList<>();
+//        //from1to100.add(Integer.valueOf(5));
+//        for (int i=0; i<99; i++) {
+//            from1to100.add(i);
+//        }
+//        System.out.println("from1to100Size " + from1to100.size());
+//
+//        list.add(Integer.valueOf(5));
+//        System.out.println(list.isEmpty());
+//        System.out.println(list.get(0));
+//        for (int i=0; i<100; i++) {
+//            list.add(i);
+//        }
+//        list.removeIf(integer -> integer==5);
+//        System.out.println("listSize " + list.size());
+//
+//        ArrayList <Integer> from100to200 = new ArrayList<>();
+//        for (int i=100; i<200; i++) {
+//            from100to200.add(i);
+//        }
+//        System.out.println("from100to200Size " + from100to200.size());
+//        from1to100.addAll(from100to200);
+//        System.out.println("from1to200 after add Size " + from1to100.size());
+//
+//        for (Integer integer: from1to100) {
+//            System.out.println(integer);
+//        }
+//
+//        from1to100.clear();
 
-       Integer [] array = new Integer[4];
+        String string = " ((1234) 23 []567788{uu(uu)}oo(888))";
+        System.out.println(checkStr(string));
 
 
-        ArrayList <Integer> list = new ArrayList<>();
-        list.add(Integer.valueOf(5));
-        System.out.println(list.isEmpty());
-        System.out.println(list.get(0));
-        for (int i=0; i<100; i++) {
-            list.add(i);
-        }
-        System.out.println(list.size());
+
     }
+
+    public static boolean checkStr (String string) {
+        Stack <Byte> stack = new Stack<>();
+        byte element1 = "(".getBytes()[0];
+        byte element2 = "[".getBytes()[0];
+        byte element3 = "{".getBytes()[0];
+
+        byte element4 = ")".getBytes()[0];
+        byte element5 = "]".getBytes()[0];
+        byte element6 = "}".getBytes()[0];
+
+        for (byte letter: string.getBytes()) {
+            if (letter== element1) {
+                stack.add(letter);
+            } else if (letter== element2) {
+                stack.add(letter);
+            } else if (letter== element3) {
+                stack.add(letter);
+            } else  if (letter== element4){
+                byte peek =  stack.peek();
+                if (peek ==element1) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (letter== element5) {
+                byte peek =  stack.peek();
+                if (peek == element2) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (letter== element6) {
+                byte peek =  stack.peek();
+                if (peek ==element3) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
 }
